87222f231fa1d3f28d976853fd0a99bd1cbc202e
salihcelebi96 <celebisalih277@gmail.com>
2023-08-11 19:43:01 +0300

    3.commit

--- weather.js
+++ weather.js
@@ -14,9 +14,9 @@ veri.addEventListener("click",()=>{
       fetch(baseUrl).then(res=>res.json())
            .then(data=>{
             const {name,sys:{country},main:{temp,feels_like},weather: [{description}]}=data;
-            const weather=document.querySelector("#der").textContent=`${temp}`;
-            const feels=document.querySelector("#feel").textContent=`${feels_like}`;
-            const havadurumu=document.querySelector("#havadurumu").textContent=`${description}`;
+            const weather=document.querySelector("#der").textContent=`${temp} `;
+            const feels=document.querySelector("#feel").textContent=`Hissedilen : ${feels_like}`;
+            const havadurumu=document.querySelector("#havadurumu").textContent=` ${description}`;
             const ülke = document.querySelector(".ülkem").textContent=`${name},${country}`;
 
 
